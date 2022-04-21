53e78e73e40b9c8766da955622accc889b69faab
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-04-21 01:38:29 -0700

    BD probada

--- src/Backend.js
+++ src/Backend.js
@@ -14,6 +14,9 @@
  * Javier Alvarez
  #######################################################################################*/
 
+//  import data from './src/Backend.js'
+//  let mydata = JSON.parse(data)
+
  const express = require('express')
  const app = express()
  const { Client } = require('pg'); // npm install pg
@@ -22,13 +25,15 @@
  
  app.use(bodyParser.json())
  app.use(cors())
+
+//  let json = require('src/Backend.js')
  
 const db = new Client({
-    host: "localhost",
-    database: "localmypet_and_me",
-    user: "postgres",
+    host: "",
+    database: "",
+    user: "",
     port: 5432,
-    password: "12345",
+    password: "f",
     ssl: {
         rejectUnauthorized: false
     }
@@ -36,10 +41,10 @@ const db = new Client({
 db.connect();
 
 app.get("/testing", (req, res) => {
-    console.log("\nPROBANDO ANUNCIOS")
+    console.log("\nPROBANDO")
 
     const sql = `
-        SELECT * FROM query_user;
+        SELECT * FROM vet;
     `
     console.log(sql)
     db.query(sql, (err, row) => {
@@ -50,4 +55,5 @@ app.get("/testing", (req, res) => {
 
 app.listen(8000, () => {
     console.log('Starting MY PET AND ME in the port 8000')
+    // console.log('JSON: ' + data)
 })
