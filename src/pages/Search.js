ae856db6e15d6a761c12ec21b3087ae3eeb23608
alv18051 <alv18051@uvg.edu.gt>
2022-05-09 22:11:07 -0600

    Update filtros para busqeuda

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -17,7 +17,7 @@
  import React, { useState } from 'react';
  import OptionComponent from './OptionComponent'
  import CardComponent from './CardComponent'
- import { Heading, Button, Input, FormControl, Select  } from '@chakra-ui/react';
+ import { Heading, Button, Input, FormControl, Select, Slider, RangeSlider, RangeSliderFilledTrack, RangeSliderTrack, RangeSliderThumb, SliderTrack, SliderFilledTrack, SliderThumb, Box, SliderMark, RangeSliderMark  } from '@chakra-ui/react';
  import './search.css'
 
 
@@ -122,19 +122,33 @@ const handleSubmit = event => {
             <div className='SearchOuterContainer2'>
               <FormControl>
                 <label>Cercania</label>
-                  <Select placeholder={'-Área de busqueda'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setCercania(event.currentTarget.value)}>
-                    <option value='option1'>{'1km'}</option>
-                    <option value='option2'>{'2km'}</option>
-                    <option value='option3'>{'5km'}</option>
-                    <option value='option4'>{'10km'}</option>
-                  </Select>
+                <Slider min={0} max = {45}  defaultValue = {0} step = {15}>
+                  <SliderMark value={1} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    1 km
+                  </SliderMark>
+                  <SliderMark value={15} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    2 km
+                  </SliderMark>
+                  <SliderMark value={30} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    4 km
+                  </SliderMark>
+                  <SliderMark value={45} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    5+ km
+                  </SliderMark>
+                  <SliderTrack>
+                    <Box position='relative' right={10}/>
+                    <SliderFilledTrack bg= 'orange'/>
+                  </SliderTrack>
+                  <SliderThumb />
+                </Slider>
+                  
               </FormControl>
             </div>
 
             <div className='SearchOuterContainer2'>
               <FormControl>
                 <label>Emergencia</label>
-                  <Select placeholder={'-Representa una emergencia'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setEmergencia(event.currentTarget.value)}>
+                <Select placeholder={'-Representa una emergencia'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setEmergencia(event.currentTarget.value)}>
                     <option value='true'>{'Si'}</option>
                     <option value='false'>{'No'}</option>
                   </Select>
@@ -144,36 +158,61 @@ const handleSubmit = event => {
             <div className='SearchOuterContainer2'>
               <FormControl>
                 <label>Tarifas</label>
-                  <Select placeholder={'-Rango de precio'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setTarifas(event.currentTarget.value)}>
-                    <option value='option1'>{'Menos de Q1,000'}</option>
-                    <option value='option2'>{'Entre Q1,000 y Q2,499'}</option>
-                    <option value='option3'>{'Entre Q2,500 y Q3,999'}</option>
-                    <option value='option4'>{'Más de Q4,000'}</option>
-                  </Select>
+                <RangeSlider aria-label={['min', 'max']}  defaultValue = {[0, 20]} step = {20}>
+                  <RangeSliderTrack>
+                    <Box position='relative' right={10}/>
+                    <RangeSliderFilledTrack bg= 'orange'/>
+                  </RangeSliderTrack>
+                  <RangeSliderThumb index={0}/>
+                  <RangeSliderThumb index={1}/>
+                </RangeSlider>
               </FormControl>
             </div>
 
             <div className='SearchOuterContainer2'>
               <FormControl>
                 <label>Rating</label>
-                  <Select placeholder={'-Valoración por estrellas'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setRating(event.currentTarget.value)}>
-                    <option value='100'>{'Cualquier valoración'}</option>
-                    <option value='100'>{'2 a 3 estrellas'}</option>
-                    <option value='100'>{'4 a 5 estrellas'}</option>
-                    <option value='100'>{'Únicamente 5 estrellas'}</option>
-                  </Select>
+                <RangeSlider aria-label={['min', 'max']}  defaultValue = {[0, 20]} step = {20}>
+                  <RangeSliderMark value={1} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    1-3
+                  </RangeSliderMark>
+                  
+                  <RangeSliderTrack>
+                    <Box position='relative' right={10}/>
+                    <RangeSliderFilledTrack bg = ' orange'/>
+                  </RangeSliderTrack>
+                  <RangeSliderThumb index={0}/>
+                  <RangeSliderThumb index={1}/>
+                </RangeSlider>
               </FormControl>
             </div>
 
             <div className='SearchOuterContainer2'>
               <FormControl>
-                <label>c de veterinarios</label>
-                  <Select placeholder={'-No. de veterinarios en turno'} focusBorderColor={'rgb(174 213 142)'} onChange ={event => setCantidad(event.currentTarget.value)}>
-                    <option value='option1'>{'1 veterinario'}</option>
-                    <option value='option2'>{'2 a 4 veterinarios'}</option>
-                    <option value='option3'>{'4 a 6 veterinarios'}</option>
-                    <option value='option4'>{'7 o más veterinarios'}</option>
-                  </Select>
+                <label>Cant. de veterinarios</label>
+                <Slider min={0} max = {50}  defaultValue = {[0]} step = {10}>
+                  <SliderMark value={10} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    1 
+                  </SliderMark>
+                  <SliderMark value={20} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    1-2 
+                  </SliderMark>
+                  <SliderMark value={30} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    2-4 
+                  </SliderMark>
+                  <SliderMark value={40} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    5-7 
+                  </SliderMark>
+                  <SliderMark value={50} mt = '1' ml = '-2.5' fontSize='smaller'>
+                    7+
+                  </SliderMark>
+                  
+                  <SliderTrack>
+                    <Box position='relative' right={10}/>
+                    <SliderFilledTrack bg= 'orange'/>
+                  </SliderTrack>
+                  <SliderThumb />
+                </Slider>
               </FormControl>
             </div>
 
@@ -232,4 +271,3 @@ const handleSubmit = event => {
    }
    
  export default Search;
-   
